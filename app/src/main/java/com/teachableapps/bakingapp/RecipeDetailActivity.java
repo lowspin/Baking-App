7af09df5fe723ef25016e2ea4026533927a0a830
tzeping <lowspin@gmail.com>
2018-07-28 18:48:32 -0400

    two fragments in one recipe detail activity.

--- app/src/main/java/com/teachableapps/bakingapp/RecipeDetailActivity.java
+++ app/src/main/java/com/teachableapps/bakingapp/RecipeDetailActivity.java
@@ -1,6 +1,7 @@
 package com.teachableapps.bakingapp;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
